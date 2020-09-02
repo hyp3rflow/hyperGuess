f76c8711998cc7a5be04e185a046f30133f59f73
hyperflow <hyperflow@kakao.com>
2020-09-02 16:12:29 +0900

    [Build]: Create header 'hyperGuess.hpp'

---
+++ Includes/hyperGuess/hyperGuess.hpp
@@ -0,0 +1,13 @@
+//
+// Created by flow on 2020-09-01.
+//
+
+#ifndef HYPERGUESS_HPP
+#define HYPERGUESS_HPP
+
+#include <hyperGuess/Agents/IAgent.hpp>
+#include <hyperGuess/Agents/RandomAgent.hpp>
+#include <hyperGuess/Enums/GameEnums.hpp>
+#include <hyperGuess/Games/Game.hpp>
+
+#endif//HYPERGUESS_HPP
