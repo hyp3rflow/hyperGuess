0e2513bdc2ac6d96e3135ccdc6b71425899bbfc4
hyperflow <hyperflow@kakao.com>
2020-09-02 16:09:16 +0900

    [Feat](Core): Create header 'GameEnums.hpp'
    Add enum 'ResultType', 'PlayState'

---
+++ Includes/hyperGuess/Enums/GameEnums.hpp
@@ -0,0 +1,24 @@
+//
+// Created by flow on 2020-09-01.
+//
+
+#ifndef HYPERGUESS_GAMEENUMS_HPP
+#define HYPERGUESS_GAMEENUMS_HPP
+
+namespace hyperguess {
+    //! \brief An enumerator for identifying the result type.
+    enum class ResultType {
+        UP,
+        EQUAL,
+        DOWN
+    };
+
+    //! \brief An enumerator for identifying the play state.
+    enum class PlayState {
+        INVALID,
+        PLAYING,
+        WON
+    };
+}// namespace hyperguess
+
+#endif//HYPERGUESS_GAMEENUMS_HPP
